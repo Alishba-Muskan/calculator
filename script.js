37c5f3423def37e2670174f16337750ad16c438a
unknown <shary8748@gmail.com>
2025-02-19 04:24:43 -0800

    modified files

--- script.js
+++ script.js
@@ -19,3 +19,127 @@ equals.addEventListener ("click", () => {
 clear.addEventListener("click", () => {
     display.value = "0"
 })
+
+
+
+
+
+
+// console.log("one");
+// console.log("two");
+
+// setTimeout(() => {
+//     console.log("hello")
+// }, 4000);
+
+// console.log("three");
+// console.log("four");
+
+
+// setTimeout(() => {
+//     console.log("hello")
+// }, 3000)
+
+// function first() {
+//     setTimeout(() => {
+//         console.log("First step done");  
+//     }, 1000);
+// }
+
+// function second() {
+//     setTimeout(() => {
+//         console.log("Second step done");
+        
+//     }, 1000);
+// }
+
+// function third() {
+//     setTimeout(() => {
+//         console.log("Third step done");
+        
+//     }, 1000);
+// }
+
+// first(() => {
+//     second(() => {
+//         second(() => {
+//             second(() => {
+//                 second();
+//             });
+//         });
+//     });
+// })
+
+
+
+
+// let promise = new Promise((resolve, reject) => {
+//     reject("error")
+// })
+
+
+
+
+    function first1() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+              console.log("First step done");  
+              resolve("success")
+          }, 5000);
+         })
+      }
+    //   function first2() {
+    //     return new Promise((resolve, reject) => {
+    //       setTimeout(() => {
+    //         console.log("second step done");  
+    //           resolve("success")
+    //       }, 5000);
+    //      })
+    //   }
+
+
+
+    //   first1().then((res) => {
+    //     first2().then((res) => {
+    //         console.log("ok")
+    //     })
+    //   })
+
+    // first1()
+    // .then((res) => {
+    //     return first2()
+    // })
+    // .then((res) => {
+    //     console.log ("done")
+    // })
+
+
+
+    
+
+
+    // let a = fetch("https://fakestoreapi.com/products")
+    // a.then((data) => {
+    //     return data.json()
+    // }).then((res) => {
+    //     console.log(res)
+    // })
+
+
+    
+    let inputvalue = document.getElementById("a")
+    let products = ["fridge", "machine", "fans", "microwave", "ac", "printer", "cooler"]
+
+
+    inputvalue.addEventListener ("keyup", (e) => {
+        let a = products.find((value) => {
+            return value.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+        console.log(a);
+        if (a) {
+            ""
+        }else{
+            alert("No record found")
+        }
+    })
+
